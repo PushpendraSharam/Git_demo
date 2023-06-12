79f344dfef9de5b52eaa79c3dbeff4503cad523a
Pushpendra <azadsharma343@gmail.com>
2023-06-12 11:30:22 +0530

    This is a commit

--- app/src/main/java/be/teletask/onvif/git_demo/MainActivity.java
+++ app/src/main/java/be/teletask/onvif/git_demo/MainActivity.java
@@ -3,6 +3,7 @@ package be.teletask.onvif.git_demo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView=findViewById(R.id.tv_view);
+        textView.setText("This is a textView");
     }
 }
